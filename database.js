89ca55f796a55d223c20bc7db11adc1a89db43c8
YuYuQiQi <88936345+YuYuQiQi@users.noreply.github.com>
2022-01-09 21:07:52 +0800

    01092107

---
+++ database.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtHxGS1_WLE6PLnqUow6xcz-YxxFUbl6Q",
+  authDomain: "webdesign-finalproject.firebaseapp.com",
+  projectId: "webdesign-finalproject",
+  storageBucket: "webdesign-finalproject.appspot.com",
+  messagingSenderId: "552532244260",
+  appId: "1:552532244260:web:48dd9f80a378986bf9e5b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
